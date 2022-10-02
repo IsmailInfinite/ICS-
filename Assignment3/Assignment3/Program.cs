864cdee0d77bd9fe9ceb34db91615bcdfc987d8d
Ismail Ahmed <ismaila@infinite.com>
2022-10-02 23:31:57 +0530

    ass3

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class miabank
+    {
+        private double balance = 1;
+
+
+        public double bal
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+    }
+    class functions
+    {
+        miabank i = new miabank();
+        string name;
+        int account;
+        double withdraw, deposit, total;
+
+        public void dep1()
+        {
+            Console.WriteLine("Enter Name of the depositor :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Account Number  :");
+            account = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Deposit Amount :");
+            deposit = Convert.ToDouble(Console.ReadLine());
+            total = i.bal + deposit;
+            Console.WriteLine("Name of the depositor : " + name);
+            Console.WriteLine("Account Number: " + account);
+            Console.WriteLine("Total Balance amount in the account  : " + total);
+        }
+        public void dep2()
+        {
+            Console.WriteLine("Enter Account Name :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Account Number  :");
+            account = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Withdraw Amount :");
+            withdraw = Convert.ToDouble(Console.ReadLine());
+            if (withdraw <= i.bal)
+            {
+                total = i.bal - withdraw;
+                Console.WriteLine("Account Name : " + name);
+                Console.WriteLine("Account Number: " + account);
+                Console.WriteLine("After Withdraw Amount balnace is : " + total);
+            }
+            else
+                Console.WriteLine("\nWithdraw Ammount does not Exist your Account.");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char again;
+            do
+            {
+                functions k = new functions();
+                int num;
+                Console.WriteLine("Select option below.");
+                Console.WriteLine("1.Deposit. \n2.Withdraw.");
+                num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        k.dep1();
+                        break;
+                    case 2:
+                        k.dep2();
+                        break;
+                    default:
+                        Console.WriteLine("Sorry you have entered a wrong input");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue this program? (y/n)");
+                again = Convert.ToChar(Console.ReadLine());
+
+            } while (again == 'y');
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment3/School/School/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Reportcard
+{
+
+    static void Main(string[] args)
+    {
+
+      
+        int roll, marks1, marks2, marks3, marks4, marks5, total;
+
+        float percentage;
+        string n, c,s,b;
+        Console.WriteLine("Roll No :");
+        roll = Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("Enter Student Name :");
+        n = Console.ReadLine();
+        Console.WriteLine("Class :");
+        c = Console.ReadLine();
+        Console.WriteLine("Semester :");
+        s = Console.ReadLine();
+        Console.WriteLine("Branch :");
+        b = Console.ReadLine();
+        Console.WriteLine("Subject 1 Marks : ");
+        marks1 = Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("Subject 2 Marks : ");
+        marks2 = Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("Subject 3 Marks :");
+        marks3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Subject 4 Marks : ");
+        marks4 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Subject 5 Marks : ");
+        marks5 = Convert.ToInt32(Console.ReadLine());
+
+        total = marks1 + marks2 + marks3 + marks4 + marks5;
+        percentage = total / 5.0f;
+
+        Console.WriteLine("Name: {0}", n);
+        Console.WriteLine("class: {0}", c);
+        Console.WriteLine("Semister: {0}", s);
+        Console.WriteLine("Branch: {0}", b);
+        Console.WriteLine("Total Marks : " + total);
+        Console.WriteLine("Percentage : " + percentage);
+
+         if (percentage <= 35)
+        {
+            Console.WriteLine("Fail");
+        }
+        else if (percentage >= 35 && percentage <= 50)
+        {
+            Console.WriteLine("Fail");
+        }
+        else if (percentage >= 50)
+        {
+            Console.WriteLine("Passed");
+        }
+        else 
+        {
+            Console.WriteLine("Wrong data");
+        }
+        
+        Console.ReadLine();
+    }
+}
